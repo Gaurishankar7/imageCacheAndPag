2007386b95fbcc4b6664a9e409e26d4080aa8cb0
Gaurishankar Vibhute <g.shankar@aitglobalinc.com>
2020-01-07 23:51:40 +0530

    Initial Commit

---
+++ LivQuikDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LivQuikDemo
+//
+//  Created by Gaurishankar Vibhute on 07/01/20.
+//  Copyright © 2020 Gaurishankar Vibhute. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
